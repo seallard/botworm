8fb889d1daf83eaa7c70561dcda279166f92a81f
seallard <seallard95@gmail.com>
2019-01-10 10:43:42 +0100

    Search goodreads for books

--- botworm.py
+++ botworm.py
@@ -1,5 +1,10 @@
 import praw
 import re
+import requests
+import xml.etree.ElementTree as ET
+import goodreads_config
+import csv
+from operator import itemgetter
 
 reddit = praw.Reddit('bot1')
 subreddit = reddit.subreddit("suggestmeabook")
@@ -11,7 +16,6 @@ def extract_book(comment):
     
     pattern = re.compile(r"[\w']+|[.,!?;]+|\n\n")
     words_and_punctuation = pattern.findall(comment)
-
     by_indices = [i for i, x in enumerate(words_and_punctuation) if x == "by"]
     
     if len(by_indices) == 0:
@@ -30,18 +34,17 @@ def extract_book(comment):
         word = words_and_punctuation[index-title_index]
         
         # Assume title consists of words before "by" that are uppercase or keywords.
-        while word.istitle() and title_index<=limit or word in keywords:
+        while title_index<=limit and word.istitle() or word in keywords:
             title = word + " " + title
             title_index += 1
             word = words_and_punctuation[index-title_index] 
         
-        if len(title)>1:
-            books.append(title + author)
-            print(title + author)
+        if len(title)>1 and len(author)>1:
+            books.append((title, author))
 
     return books
 
-book_list = []
+books_list = []
 
 # Iterate over comments and extract titles. 
 for submission in subreddit.hot(limit=3):
@@ -51,4 +54,51 @@ for submission in subreddit.hot(limit=3):
             books = extract_book(comment.body)
             
             if books != None and books != []:
-                book_list.append(books)
+                books_list.append(books)
+
+base_url = "https://www.goodreads.com/search/index.xml?key=" + goodreads_config.api_key + "&q="
+goodreads_matches = []
+
+# Iterate over extracted titles and search goodreads.     
+for books in books_list:
+    for book in books:
+
+        title, author = book
+        query = title + author
+        
+        response = requests.get(base_url + query)
+        root = ET.fromstring(response.content)
+        
+        max_count = 0
+        match = False
+
+        for child in root.iter('work'):
+            count = int(child.findtext('ratings_count'))
+                
+            if count >= max_count:
+                match = True
+                max_count = count   
+                rating = child.findtext('average_rating')
+                year = child.findtext('original_publication_year')
+                gr_title = child.find('best_book').findtext('title')
+                book_id =  child.find('best_book').findtext('id')
+                name = child.find('best_book').find('author').findtext('name')
+
+        if match:
+            link = "https://www.goodreads.com/book/show/"+book_id+".a"
+            goodreads_matches.append((gr_title, name, int(max_count), rating, link))
+            
+            print("Search: " + query)   
+            print("Match: " + gr_title + " by " + name)
+            print("Number of reads: " + str(max_count))
+            print("--------")
+
+sorted_books = sorted(goodreads_matches, key=itemgetter(2), reverse=True)
+with open("result.csv", "w", newline='') as f:
+    
+    f.write("Title | Author | Reads | Rating | Link\n")
+    f.write(":--|:--|:--|:--|:--\n")
+    
+    for book in sorted_books:
+        title, author, reads, rating, link = book
+        f.write(("{} | {} | {} | {} | [goodreads]({})\n").format(title, author, str(reads), rating, link))
