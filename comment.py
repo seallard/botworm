701000cc142d1d00315d90ce5c2f21080fe67680
seallard <seallard95@gmail.com>
2020-05-19 17:44:47 +0200

    Missing self

--- comment.py
+++ comment.py
@@ -16,11 +16,11 @@ class Comment:
         by_indices = self.__find_by_indices()
         books = []
         for index in by_indices:
-            book_author = self.extract_author(index)
-            title = self.__extract_title(index)
+            book_author = self.__extract_author(index)
+            book_title = self.__extract_title(index)
 
-            if len(title) > 1 and len(author) > 1:
-                books.append(title + " " + author)
+            if len(book_title) > 1 and len(book_author) > 1:
+                books.append(book_title + " " + book_author)
         return books
 
 
@@ -29,34 +29,34 @@ class Comment:
         return pattern.findall(self.text)
 
 
-    def __find_by_indices(self, clean_comment):
+    def __find_by_indices(self):
         """ Return list of indices at which "by" occurs. """
-        by_indices = [i for i, x in enumerate(clean_text) if x == "by"]
+        by_indices = [i for i, x in enumerate(self.clean_text) if x == "by"]
         return by_indices
 
 
     def __extract_author(self, index):
-        return " ".join(clean_text[index+1:index+3])
+        return " ".join(self.clean_text[index+1:index+3])
 
 
     def __extract_title(self, index):
         title = ""
         title_index = 1
-        word = clean_text[index-title_index]
-        limit = len(clean_text[:index])
+        word = self.clean_text[index-title_index]
+        limit = len(self.clean_text[:index])
 
-        while title_index <= limit and word.istitle() or self.is_keyword(word):
+        while title_index <= limit and word.istitle() or self.__is_keyword(word):
             title = word + " " + title
             title_index += 1
-            word = clean_text[index-title_index]
+            word = self.clean_text[index-title_index]
 
         return title
 
 
-    def __is_key_word(self, word):
+    def __is_keyword(self, word):
         keywords = ['of', 'the', 'a', 'at', 'to']
         return word in keywords
 
 
     def __create_url(self):
-        return f"https://www.reddit.com/comments/{post_id}/_/{comment_id}/)"
+        return f"https://www.reddit.com/comments/{self.post_id}/_/{self.comment_id}/)"
