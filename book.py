04ecb87ade70a4aaabd4c10ccad1192908866932
seallard <seallard95@gmail.com>
2020-05-21 16:50:50 +0200

    Replace namedtuple with class

--- book.py
+++ book.py
@@ -1,8 +1,14 @@
-from typing import NamedTuple
 
-class Book(NamedTuple):
-    title: str
-    author: str
-    average_rating: float
-    ratings_count: int
-    goodreads_id: int
+class Book():
+
+    def __init__(self, title, author, rating, count, goodreads_id):
+        self.title = title
+        self.author = author
+        self.rating = rating
+        self.count = count
+        self.id = goodreads_id
+        self.url = self.__create_url()
+
+
+    def __create_url(self):
+        return "https://www.goodreads.com/book/show/" + str(self.id)
