1eda920ff2dc0e3155edfd33812fc6e78e27f41d
seallard <seallard95@gmail.com>
2020-05-22 11:27:22 +0200

    Format title

--- book.py
+++ book.py
@@ -2,13 +2,18 @@
 class Book():
 
     def __init__(self, title, author, rating, count, goodreads_id):
-        self.title = title
+        self.title = self.__format_title(title)
         self.author = author
         self.rating = rating
         self.count = count
         self.id = goodreads_id
         self.url = self.__create_url()
 
+
+    def __format_title(self, title):
+        """ Remove series name and subtitle. """
+        new_title = title.split("(")[0].split(":")[0].split(",")[0].split("by")[0]
+        return new_title
 
     def __create_url(self):
         return "https://www.goodreads.com/book/show/" + str(self.id)
