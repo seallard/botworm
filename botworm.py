d8b68bdd0d0d6f87eefcd7833fb97d1c9f06e4ae
seallard <seallard95@gmail.com>
2020-05-22 11:07:50 +0200

    Finish main logic

--- botworm.py
+++ botworm.py
@@ -1,5 +1,6 @@
 from reddit import Reddit
 from goodreads import Goodreads
+from recommendation import Recommendation
 
 
 if __name__ == "__main__":
@@ -9,9 +10,17 @@ if __name__ == "__main__":
 
 
     for post in reddit.get_posts():
+
+        recommendations = []
+
         for comment in reddit.get_comments(post):
+            mentioned_books = comment.get_mentioned_books()
 
-            titles = comment.get_mentioned_books()
+            for title in mentioned_books:
+                book = goodreads.get_book(title)
 
-            for query in titles:
-                book = goodreads.get_book(query)
+                if book != None:
+                    recommendations.append(Recommendation(book, comment))
+
+        tables = reddit.create_table(recommendations)
+        reddit.post_comments(post, tables)
