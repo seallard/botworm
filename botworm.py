6da45aaa3f44b17d121feb1c76e284e1906286de
seallard <seallard95@gmail.com>
2020-06-01 13:33:04 +0200

    Connect to database

--- botworm.py
+++ botworm.py
@@ -2,6 +2,7 @@ from utils.reddit import Reddit
 from utils.goodreads import Goodreads
 from utils.book_title_parser import BookTitleParser
 from utils.recommendation_lister import RecommendationLister
+from utils.database import Database
 
 
 def main():
@@ -9,6 +10,7 @@ def main():
     reddit = Reddit("bot1", "suggestmeabook")
     goodreads = Goodreads()
     title_parser = BookTitleParser()
+    database = Database()
 
     for post in reddit.get_posts():
         lister = RecommendationLister()
@@ -20,6 +22,7 @@ def main():
                 print(title)
                 book = goodreads.get_book(title)
                 lister.add(book, comment)
+                database.test()
 
         recommendations = lister.get()
         tables = reddit.create_table(recommendations)
