2bba97c22b39b0583a3fc9bbc39e483aa49f14dc
seallard <seallard95@gmail.com>
2020-06-08 13:36:25 +0200

    Add sqlalchemy properties

--- models/comment.py
+++ models/comment.py
@@ -1,13 +1,22 @@
-import re
+from sqlalchemy import Table, Column, String, Integer, Date, ForeignKey
+from sqlalchemy.orm import relationship
+from utils.database import Base
+from models.association_tables import comments_books_association
 
-class Comment:
 
-    def __init__(self, text, author, comment_id, post_id):
+class Comment(Base):
+    __tablename__ = 'comments'
+
+    id = Column(Integer, primary_key=True)
+    text = Column(String)
+    books = relationship("Book", secondary=comments_books_association, backpopulates="comments")
+
+    def __init__(self, text, author, id, post_id):
         self.text = text
         self.author = author
-        self.comment_id = comment_id
+        self.id = id
         self.post_id = post_id
         self.url = self.__create_url()
 
     def __create_url(self):
-        return f"https://www.reddit.com/comments/{self.post_id}/_/{self.comment_id}/"
+        return f"https://www.reddit.com/comments/{self.post_id}/_/{self.id}/"
