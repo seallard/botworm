6f8f91bc54a1eb043da6ed605d9beb4e55fdc079
seallard <seallard95@gmail.com>
2020-07-02 10:06:26 +0200

    Tracker functions

--- models/comment.py
+++ models/comment.py
@@ -8,19 +8,20 @@ class Comment(Base):
     __tablename__ = 'comments'
 
     id = Column(String, primary_key=True)
-    date = Column(Date, default)
+    date = Column(Date)
     post_id = Column(String, ForeignKey('posts.id'))
-    by_bot = Column(Boolean, unique=False, default=False)
+    by_bot = Column(Boolean)
 
     books = relationship("Book", secondary=comments_books_association, back_populates="comments")
 
-    def __init__(self, text, author, id, post_id, date):
+    def __init__(self, text, author, id, post_id, date, by_bot=False):
         self.text = text
         self.author = author
         self.id = id
         self.post_id = post_id
         self.url = self.__create_url()
         self.date = date
+        self.by_bot = by_bot
 
     def __create_url(self):
         return f"https://www.reddit.com/comments/{self.post_id}/_/{self.id}/"
--- utils/recommendation_tracker.py
+++ utils/recommendation_tracker.py
@@ -3,12 +3,13 @@ from sqlalchemy.sql import exists
 from models.post import RedditPost
 from models.book import Book
 from models.comment import Comment
-
+import json
 
 class RecommendationTracker:
 
     def __init__(self):
         self.session = session_factory()
+        self.__read_config()
 
     def add(self, data):
         self.session.add(data)
@@ -28,10 +29,60 @@ class RecommendationTracker:
         self.session.commit()
         self.session.close()
 
-    def post_exists(self, post_id):
+    def __post_exists(self, post_id):
         stmt = exists().where(RedditPost.id==post_id)
         post_exists = self.session.query(stmt).scalar()
         return post_exists
 
+    def comment_exists(self, comment_id):
+        stmt = exists().where(Comment.id==comment_id)
+        comment_exists = self.session.query(stmt).scalar()
+        return comment_exists
+
     def number_of_comments_stored(self, post_id):
         return self.session.query(Comment).filter(Comment.post_id == post_id).count()
+
+    def most_recent_comment_by_bot(self, post_id):
+        return self.session.query(Comment).\
+                    filter(Comment.post_id == post_id).\
+                    filter(Comment.by_bot == True).\
+                    order_by(Comment.date.desc()).first()
+
+    def filter_posts(self, posts):
+        filtered_posts = []
+        for post in posts:
+            if self.__post_should_be_checked(post):
+                filtered_posts.append(post)
+        return filtered_posts
+
+    def filter_comments(self, comments):
+        post_id = comments[0].post_id
+
+        # If no table has been posted, all comments are to be checked.
+        if not self.most_recent_comment_by_bot(post_id):
+            return comments
+
+        filtered_comments = []
+        for comment in comments:
+            if self.__comment_should_be_checked(comment):
+                filtered_comments.append(comment)
+        return filtered_comments
+
+    def __post_should_be_checked(self, post):
+        if not self.__post_exists(post.id):
+            return True
+
+        old = self.number_of_comments_stored(post.id)
+        new = post.num_comments
+
+        if new/old >= self.new_comments_ratio:
+            return True
+        return False
+
+    def __comment_should_be_checked(self, comment):
+        return not self.comment_exists(comment.id)
+
+    def __read_config(self):
+        with open('configs/config.json') as config_file:
+                config = json.load(config_file)
+                self.new_comments_ratio = config["new_comments_ratio"]
